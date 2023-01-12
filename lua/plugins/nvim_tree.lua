b91b946665b2511bc85f022fb40451970c301db0
Carpincho99 <ezegiorgis@hotmail.com>
2023-01-11 23:59:51 -0300

    config nvimTree

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,61 +1,59 @@
 return {
   'kyazdani42/nvim-tree.lua',
+  event = "VimEnter",
   keys = {
-    {'<leader>e', "<cmd>NvimTreeToggle<cr>" , desc="Open tree" }
-  },
-  opts = {
-    update_focused_file = {
-      enable = true,
-      update_cwd = true,
-    },
-    renderer = {
-      root_folder_modifier = ":t",
-      icons = {
-        glyphs = {
-          default = "",
-          symlink = "",
-          folder = {
-            arrow_open = "",
-            arrow_closed = "",
-            default = "",
-            open = "",
-            empty = "",
-            empty_open = "",
-            symlink = "",
-            symlink_open = "",
-          },
-          git = {
-            unstaged = "",
-            staged = "S",
-            unmerged = "",
-            renamed = "➜",
-            untracked = "U",
-            deleted = "",
-            ignored = "◌",
-          },
-        },
-      },
-    },
-    diagnostics = {
-      enable = true,
-      show_on_dirs = true,
-      icons = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
-      },
-    },
-
-    actions = {
-      open_file = {
-        quit_on_open = true,
-      },
-    },
+    { '<leader>e', "<cmd>NvimTreeToggle<cr>", desc = "Open tree" }
   },
   config = function()
 
-    require('nvim-tree').setup{
+    require('nvim-tree').setup {
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      renderer = {
+        root_folder_modifier = ":t",
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+              arrow_open = "",
+              arrow_closed = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "U",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      },
       view = {
         width = 30,
         side = "left",
